ed79fa85654a357f3728a29b823c01bc755347f6
Arven <brian.johnathan.b@gmail.com>
2015-04-08 09:24:45 -0400

    Use producer built into Weld to handle Config Bean

--- weld-cxf-jaxrs/src/main/java/io/github/arven/flare/boot/FlareApplication.java
+++ weld-cxf-jaxrs/src/main/java/io/github/arven/flare/boot/FlareApplication.java
@@ -6,45 +6,71 @@
 package io.github.arven.flare.boot;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import com.wordnik.swagger.config.Scanner;
 import com.wordnik.swagger.config.ScannerFactory;
 import com.wordnik.swagger.converter.ModelConverters;
-import com.wordnik.swagger.jaxrs.config.ReflectiveJaxrsScanner;
 import io.github.arven.flare.ee.WeldFlare;
 import io.github.arven.flare.server.jetty.FlareJettyServer;
 import io.github.arven.flare.server.FlareServer;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Set;
+import javax.enterprise.util.AnnotationLiteral;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
 
 /**
- *
- * @author brian.becker
+ * The FlareApplication main class, which starts the web application,
+ * loads configuration from the main class, and loads a few beans from
+ * the Weld container which will be used in configuration.
+ * 
+ * @author Brian Becker
  */
 public class FlareApplication {
-        
+           
     public void run(String[] args) {
         try {
             String webappDir = "./src/main/webapp/";
             
-            FlareNamingConfiguration naming = new FlareNamingConfiguration();
-            naming.configure();
+            Reflections reflections = new Reflections("");
+            Set<Class<?>> cls = reflections.getTypesAnnotatedWith(FlareBootApplication.class);
+            if(cls.size() != 1) {
+                throw new RuntimeException("You must have a single @FlareBootApplication annotated class");
+            }
+            Class<?> main = cls.iterator().next();
+            Package pkg = main.getPackage();
             
+            Object config = main.newInstance();
+            
+            reflections = new Reflections(pkg.getName(), new MethodAnnotationsScanner());
+
+            Set<Method> naming = reflections.getMethodsAnnotatedWith(NamingConfiguration.class);
+            for(Method m : naming) {
+                Context ic = new InitialContext();
+                try {
+                    ic.createSubcontext("java:comp/env/");
+                } catch(NamingException ex) {}
+                m.invoke(config, ic);
+            }
+
             WeldFlare weld = new WeldFlare();
-            weld.initialize();
+            WeldContainer container = weld.initialize();
 
             FlareServer server = new FlareJettyServer(webappDir);
             server.init();
             server.start();
-
-            ReflectiveJaxrsScanner scanner = new ReflectiveJaxrsScanner();
-            scanner.setResourcePackage("io.github.arven.rs.services.example");
-            ScannerFactory.setScanner(scanner);
-
-            ObjectMapper obMap = new ObjectMapper();
-            obMap.setAnnotationIntrospector(new JaxbAnnotationIntrospector(obMap.getTypeFactory()));
-            ModelConverters.getInstance().addConverter(new com.wordnik.swagger.jackson.ModelResolver(obMap));
-
+            
+            ScannerFactory.setScanner(container.instance().select(Scanner.class, new AnnotationLiteral<FlareConfiguration>() {}).get());
+            ModelConverters.getInstance().addConverter(new com.wordnik.swagger.jackson.ModelResolver(container.instance().select(ObjectMapper.class, new AnnotationLiteral<FlareConfiguration>() {}).get()));
+            
             server.join();
 
             weld.shutdown();
+            
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- weld-cxf-jaxrs/src/main/java/io/github/arven/rs/services/example/weld/Application.java
+++ weld-cxf-jaxrs/src/main/java/io/github/arven/rs/services/example/weld/Application.java
@@ -1,7 +1,16 @@
 package io.github.arven.rs.services.example.weld;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import com.wordnik.swagger.config.Scanner;
+import com.wordnik.swagger.jaxrs.config.ReflectiveJaxrsScanner;
 import io.github.arven.flare.boot.FlareApplication;
 import io.github.arven.flare.boot.FlareBootApplication;
+import io.github.arven.flare.boot.FlareConfiguration;
+import io.github.arven.flare.boot.NamingConfiguration;
+import javax.enterprise.inject.Produces;
+
+import javax.naming.Context;
 
 /**
  * This Weld Boot application configuration simply defines Jersey and
@@ -15,6 +24,25 @@ public class Application {
     public static void main(String[] args) {
         FlareApplication app = new FlareApplication();
         app.run(args);
+    }
+    
+    @NamingConfiguration
+    public void configureNaming(Context context) throws Exception {
+        context.bind("java:comp/env/mySpecialValue", 4000);
+    }
+    
+    @Produces @FlareConfiguration
+    public Scanner configureSwagger() {
+        ReflectiveJaxrsScanner scanner = new ReflectiveJaxrsScanner();
+        scanner.setResourcePackage("io.github.arven.rs.services.example");
+        return scanner;
+    }
+    
+    @Produces @FlareConfiguration
+    public ObjectMapper configureJackson() {
+        ObjectMapper obMap = new ObjectMapper();
+        obMap.setAnnotationIntrospector(new JaxbAnnotationIntrospector(obMap.getTypeFactory()));        
+        return obMap;
     }
     
 }
